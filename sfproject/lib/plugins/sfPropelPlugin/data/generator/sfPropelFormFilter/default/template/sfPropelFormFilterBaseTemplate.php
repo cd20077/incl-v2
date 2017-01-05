7eff372124bac097c898ce0871385bc9493b83f8
cd <nhs20077+git@gmail.com>
2017-01-05 09:57:02 +0000

    wip

---
+++ sfproject/lib/plugins/sfPropelPlugin/data/generator/sfPropelFormFilter/default/template/sfPropelFormFilterBaseTemplate.php
@@ -0,0 +1,15 @@
+[?php
+
+/**
+ * Project filter form base class.
+ *
+ * @package    inclv2
+ * @subpackage filter
+ * @author     Your name here
+ */
+abstract class BaseFormFilterPropel extends sfFormFilterPropel
+{
+  public function setup()
+  {
+  }
+}
